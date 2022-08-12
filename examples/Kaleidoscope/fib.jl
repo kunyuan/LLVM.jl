398f1aee2d60ed253c8665b09f589287c2b77d50
Kun Chen <chenkun0228@gmail.com>
2022-08-12 12:05:14 -0400

    make fib.jl more robust

--- examples/Kaleidoscope/fib.jl
+++ examples/Kaleidoscope/fib.jl
@@ -12,29 +12,35 @@ using LLVM.Interop
 
 include("./Kaleidoscope.jl")
 
-ctx = Context()
-
 #you may also use the default context of the current julia session
 # ctx = GlobalContext()
 
-mod = Kaleidoscope.generate_IR(source; ctx=ctx)
-Kaleidoscope.optimize!(mod)
-
-########### to run in julia ###########################
-engine = Interpreter(mod)
-f = LLVM.functions(mod)["fib"] #get the fib function
-args = [GenericValue(LLVM.DoubleType(LLVM.context(mod)), 10.0),]
-#take 0.000151 seconds (4 allocations: 176 bytes)
-# slow!
-res = LLVM.run(engine, f, args)
-res_jl = convert(Float64, res, LLVM.DoubleType(LLVM.context(mod)))
-println(res_jl)
+@dispose ctx = Context() begin
 
 
-############ make the function callable from julia ##############
+    mod = Kaleidoscope.generate_IR(source; ctx=ctx)
+    Kaleidoscope.optimize!(mod)
 
-@eval call_fib(x) = $(call_function(f, Float64, Tuple{Float64,}, :x))
-#take 0.000002 seconds, no allocations
-# very fast!
+    ########### to run in julia ###########################
+    @dispose engine = Interpreter(mod) begin
+        f = LLVM.functions(mod)["fib"] #get the fib function
+        args = [GenericValue(LLVM.DoubleType(LLVM.context(mod)), 10.0),]
+        #take 0.000151 seconds (4 allocations: 176 bytes)
+        # slow!
+        res = LLVM.run(engine, f, args)
+        res_jl = convert(Float64, res, LLVM.DoubleType(LLVM.context(mod)))
+        println(res_jl)
+
+        ############ make the function callable from julia ##############
+
+        @eval call_fib(x) = $(call_function(f, Float64, Tuple{Float64,}, :x))
+        #take 0.000002 seconds, no allocations
+        # very fast!
+
+        dispose.(args)
+        dispose(res)
+    end
+end
+
 res2 = call_fib(10.0)
 println(res2)
