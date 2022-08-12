48586325ae5904e4bd97263e46bb37fb11053d5d
Kun Chen <chenkun0228@gmail.com>
2022-08-12 12:09:39 -0400

    make fib.jl more robust

--- examples/Kaleidoscope/fib.jl
+++ examples/Kaleidoscope/fib.jl
@@ -19,20 +19,22 @@ include("./Kaleidoscope.jl")
 
 
     mod = Kaleidoscope.generate_IR(source; ctx=ctx)
+    # you can get current context by calling LLVM.context(mod)
     Kaleidoscope.optimize!(mod)
 
     ########### to run in julia ###########################
     @dispose engine = Interpreter(mod) begin
         f = LLVM.functions(mod)["fib"] #get the fib function
-        args = [GenericValue(LLVM.DoubleType(LLVM.context(mod)), 10.0),]
+        args = [GenericValue(LLVM.DoubleType(ctx), 10.0),]
         #take 0.000151 seconds (4 allocations: 176 bytes)
         # slow!
         res = LLVM.run(engine, f, args)
-        res_jl = convert(Float64, res, LLVM.DoubleType(LLVM.context(mod)))
+        res_jl = convert(Float64, res, LLVM.DoubleType(ctx))
         println(res_jl)
 
         ############ make the function callable from julia ##############
 
+        push!(function_attributes(f), EnumAttribute("alwaysinline"; ctx))
         @eval call_fib(x) = $(call_function(f, Float64, Tuple{Float64,}, :x))
         #take 0.000002 seconds, no allocations
         # very fast!
